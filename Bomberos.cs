22317652c3c8d36c94d13c4c890c032cc99c706f
Hugo-cell70 <85138145+Hugo-cell70@users.noreply.github.com>
2022-01-26 11:40:29 -0300

    Add files via upload

---
+++ Bomberos.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Bomberos
+{
+    #region Bomberos
+    public class Bomberos
+    {
+        #region Member Variables
+        protected string _BomberoEmp;
+        #endregion
+        #region Constructors
+        public Bomberos() { }
+        public Bomberos()
+        {
+        }
+        #endregion
+        #region Public Properties
+        public virtual string BomberoEmp
+        {
+            get {return _BomberoEmp;}
+            set {_BomberoEmp=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
